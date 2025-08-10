0c15566de69177f20a559bc3da4f14e6577f995a
Pugavkomm <slavapugavko2@gmail.com>
2025-08-10 19:57:43 -0300

    Add `SessionDep` dependency

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
-from typing import Any, AsyncGenerator
+from typing import Any, AsyncGenerator, Annotated
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
