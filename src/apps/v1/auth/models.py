4f6ec0a9957e9f2b35c06ec9842cc3fe48a436a4
Pugavkomm <slavapugavko2@gmail.com>
2025-09-01 03:08:17 -0300

    Add some properties to usermodel

--- src/apps/v1/auth/models.py
+++ src/apps/v1/auth/models.py
@@ -1,13 +1,32 @@
 from uuid import UUID, uuid4
 
+from passlib.context import CryptContext
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.database import Base
+from src.database import Base, pk_uuid, str_256
+
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    default="pbkdf2_sha256",
+    pbkdf2_sha256__default_rounds=30000,
+)
 
 
 class UserModel(Base):
     __tablename__ = "user"
-    user_uuid: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    user_uuid: Mapped[pk_uuid]
     username: Mapped[str] = mapped_column(String(length=100), unique=True)
-    password_hash: Mapped[str] = mapped_column(String(200))
+    password_hash: Mapped[str_256] = mapped_column(name="password_hash")
+    email: Mapped[str_256] = mapped_column(unique=True, nullable=True)
+
+    @property
+    def password(self) -> None:
+        raise AttributeError("Password is not a readable")
+
+    @password.setter
+    def password(self, password: str) -> None:
+        self.password_hash = pwd_context.hash(password)
+
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.password_hash)
