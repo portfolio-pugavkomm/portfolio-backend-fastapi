c1d387a3a8337e98453f3d7b1ebf94b4aa750e73
Pugavkomm <slavapugavko2@gmail.com>
2025-09-07 22:21:27 -0300

    Fix table name (drop table!)

--- src/apps/v1/resources/models.py
+++ src/apps/v1/resources/models.py
@@ -4,7 +4,11 @@ from src.database import Base, pk_int, str_256
 
 
 class ResourceSkillModel(Base):
-    __tablename__ = "resourse_skills"
+    __tablename__ = "resource_skills"
     id: Mapped[pk_int]
     name: Mapped[str_256]
     weight: Mapped[int] = mapped_column(default=0)
+
+
+# class ResourceWorkTimeLineItemModel(Base):
+#     __tablename__ = "wor"
